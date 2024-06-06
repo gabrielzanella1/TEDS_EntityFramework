26c84436590e0264192fd1a10c9600a6737a174a
Gabriel <gzanella202@gmail.com>
2024-06-06 16:10:32 -0300

    Exercício Tópicos

---
+++ models/Cliente.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Loja.models
+{
+    public class Cliente
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+    }
+}
